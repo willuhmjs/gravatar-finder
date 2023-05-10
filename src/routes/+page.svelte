30c4fda6d33f540a36bd73ad5f3a0560ae7e8cf4
William Faircloth <willuhmjs@gmail.com>
2023-05-10 01:28:46 +0000

    feat: basic working app

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    let email: string = "";
+    $: hashedEmail = CryptoJS.MD5(email.replace(/\s/g, "").toLowerCase());
+</script>
+
+<input type="email" bind:value={email}/>
+<input type="text" disabled value="ttps://www.gravatar.com/avatar/{hashedEmail}"/>
+<img src="https://www.gravatar.com/avatar/{hashedEmail}?s=300" alt="gravatar preview"/>
+
